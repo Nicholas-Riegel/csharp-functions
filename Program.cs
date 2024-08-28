3eedc871fb8f24573d6e222f4d09696a05dd8f4b
Nicholas Riegel <nicholaspriegel@gmail.com>
2024-08-28 09:25:59 -0400

    init

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System.Runtime.InteropServices;
+using System.Text;
+using classes;
+
+// Test.TestMessage();
+
+string test = "hello";
+string test2 = test.Replace("e", "a");
+
+// STRINGBUILDER
+StringBuilder sb = new("Hello, ");
+sb.Append("world!");
+sb.Insert(7, "beautiful ");
+sb.Replace("world", "C#");
+sb.Remove(7, 10); // Removes "beautiful"
+
+// Console.WriteLine(sb.ToString()); // Outputs "Hello, C#!"
+
+// FUNCTION PARAMETERS
+// functional overloading is when have same name for a function, but can give it different parameters
+// static double Add(int a, int b = 0)
+// static double Add(int a, int b = default) // gives the default value for the number
+// static double Add(int a, [Optional] int b) // gives the default value for the number
+// {
+//     System.Console.WriteLine(b);
+//     return a + b;
+// }
+
+// System.Console.WriteLine(Add(5));
+
+// Named parameters
+// string nameInput = "Aba";
+// int ageInput = 23;
+// string addressInput = "1 Something road";
+
+// static void PrintDetails(string name, int age, string address)
+// {
+//     System.Console.WriteLine($"Name: {name}");
+//     System.Console.WriteLine($"Age: {age}");
+//     System.Console.WriteLine($"Address: {address}");
+// }
+
+// PrintDetails(age:ageInput, name:nameInput, address:addressInput);
+
+// OUT PARAMETERS
+
+// int num = 0;
+
+// static void testFn(out int x)
+// {
+//     x = 5;
+// }
+
+// testFn(out num); // don't have to specify 'int' bc already declared
+
+// Console.WriteLine(num); //=5
+
+List<string> shoppingList = ["coffee", "milk"];
+
+// System.Console.WriteLine(shoppingList.IndexOf("milk"));
+
+int a = 1;
+int b = 1;
+
+if (a.Equals(b))
+{
+    System.Console.WriteLine("true");
+} 
+else
+{
+    System.Console.WriteLine("false");
+}
---
+++ Test.cs
@@ -0,0 +1,9 @@
+namespace classes;
+
+class Test
+{
+    public static void TestMessage()
+    {
+        Console.WriteLine("test message");
+    }
+}
